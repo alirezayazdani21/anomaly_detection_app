f6ec592e3ad2dd9820539e757f4873f197585fcb
alirezayazdani21 <alirezayazdani21@yahoo.com>
2023-11-30 21:09:39 -0500

    git config --global core.editor "code --wait"

--- ts_anomaly_detector_v03.py
+++ ts_anomaly_detector_v03.py
@@ -13,7 +13,6 @@ import plotly.express as px
 def load_data(file):
     df = pd.read_csv(file)
     return df
-
 ###################################################################
 
 # Streamlit app
